f6af2f4d3eabbd1dbca667cca8a9d1322acf2570
Reider47 <71041827+Reider47@users.noreply.github.com>
2023-09-04 11:45:01 +0200

    Added all the files and setup from the portfolio project

---
+++ Client/Data/PortfolioItem.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorBasic.Data
+{
+    public class PortfolioItem
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public string imgLink { get; set; }
+
+        public PortfolioItem(string name, string description) 
+        {
+            this.name = name;
+            this.description = description;
+            imgLink = "dc_icon2.svg";
+        }
+        public PortfolioItem(string name, string description, string imgLink)
+        {
+            this.name = name;
+            this.description = description;
+            this.imgLink = imgLink;
+        }
+    }
+}
