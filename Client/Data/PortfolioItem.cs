56c1a8d876d85bf3ee4f3e495989de2c5e94dd0d
Reider47 <71041827+Reider47@users.noreply.github.com>
2023-09-08 10:02:52 +0200

    Added links to the github repositorioes on the portfolio section.

--- Client/Data/PortfolioItem.cs
+++ Client/Data/PortfolioItem.cs
@@ -4,19 +4,28 @@
     {
         public string name { get; set; }
         public string description { get; set; }
-        public string imgLink { get; set; }
+        public string imgPath { get; set; }
 
+        public string link { get; set; }
         public PortfolioItem(string name, string description) 
         {
             this.name = name;
             this.description = description;
-            imgLink = "dc_icon2.svg";
+            imgPath = "dc_icon2.svg";
         }
-        public PortfolioItem(string name, string description, string imgLink)
+        public PortfolioItem(string name, string description, string link)
         {
             this.name = name;
             this.description = description;
-            this.imgLink = imgLink;
+            this.imgPath = "dc_icon2.svg";
+            this.link = link;
+        }
+        public PortfolioItem(string name, string description, string imgPath, string link)
+        {
+            this.name = name;
+            this.description = description;
+            this.imgPath = imgPath;
+            this.link = link;
         }
     }
 }
